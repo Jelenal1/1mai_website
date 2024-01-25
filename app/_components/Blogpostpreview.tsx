2ec217d94805ab2fe3af38a362ca5ee6bb8e4793
jelenal1 <jabbruh10@gmail.com>
2024-01-25 21:39:36 +0100

    BlogPost preview

---
+++ app/_components/Blogpostpreview.tsx
@@ -0,0 +1,15 @@
+export default function Blogpostpreview() {
+  return (
+    <div className="flex min-w-[300px] max-w-[400px] flex-col rounded-xl border border-black p-2 text-center">
+      <h2 className="text-xl">Blogpostpreview</h2>
+      <h3>Author</h3>
+      <img src="//via.placeholder.com/300" alt="" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quam
+        dolores et placeat, laudantium laboriosam sit temporibus quis, eos cum
+        ratione nihil illo veniam itaque, modi aspernatur asperiores dignissimos
+        laborum?
+      </p>
+    </div>
+  );
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+import Blogpostpreview from "../_components/Blogpostpreview";
+export default function Blog() {
+  return (
+    <main className="mx-9 mb-10 flex flex-col items-center">
+      <h2 className="mb-3 text-xl font-bold lg:text-2xl">Blog</h2>
+      <Blogpostpreview />
+    </main>
+  );
+}
