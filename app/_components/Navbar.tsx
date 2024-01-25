ce001c7ef884da63907304cbec55246436ce848c
jelenal1 <jabbruh10@gmail.com>
2024-01-25 20:45:00 +0100

    style: navbar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { redirect } from "next/navigation";
 import { useState } from "react";
 import { FiMenu } from "react-icons/fi";
 import Sidenav from "./Sidenav";
@@ -7,20 +6,40 @@ import Link from "next/link";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+  const [openArchiv, setOpenArchiv] = useState(false);
 
   return (
-    <div className="sticky top-0 z-10 flex h-20 w-full items-center border-b-2 border-black px-2 backdrop-blur ">
-      <h1 className="ml-2 text-2xl hover:cursor-pointer">1 Mai Winterthur</h1>
-      <button className="ml-auto mr-2 lg:hidden" onClick={() => setOpen(true)}>
-        <FiMenu size={30} />
-      </button>
-      <Sidenav open={open} setOpen={setOpen} />
-      <div className="ml-20 hidden gap-10 text-xl lg:flex">
-        <Link href={"/"}>Home</Link>
-        <Link href={"/mitmachen"}>Mitmachen</Link>
-        <Link href={"/spenden"}>Spenden</Link>
-        <Link href={"/ueber-uns"}>Über uns</Link>
-        <Link href={"/archive"}>Archiv</Link>
+    <div className="sticky top-0 z-10 flex min-h-[100px] w-full flex-col border-b-2 border-black px-2 backdrop-blur ">
+      <div className="mt-8 flex w-full items-center">
+        <Link href={"/"}>
+          <h1 className="ml-2 text-2xl hover:cursor-pointer">
+            1 Mai Winterthur
+          </h1>
+        </Link>
+        <button
+          className="ml-auto mr-2 lg:hidden"
+          onClick={() => setOpen(true)}
+        >
+          <FiMenu size={30} />
+        </button>
+        <Sidenav open={open} setOpen={setOpen} />
+        <div className="ml-20 hidden gap-10 text-xl lg:flex">
+          <Link href={"/"}>Home</Link>
+          <Link href={"/mitmachen"}>Mitmachen</Link>
+          <Link href={"/spenden"}>Spenden</Link>
+          <Link href={"/ueber-uns"}>Über uns</Link>
+          <button onClick={() => setOpenArchiv(!openArchiv)}>Archiv</button>
+        </div>
+      </div>
+      <div
+        className={
+          openArchiv ? "mb-5 ml-[740px] hidden lg:flex lg:flex-col" : "hidden"
+        }
+      >
+        <Link href={"/archive/2023"} className="my-2">
+          2023
+        </Link>
+        <Link href={"/archive/2023"}>2022</Link>
       </div>
     </div>
   );
