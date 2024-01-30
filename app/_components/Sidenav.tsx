e5b840a9411fc5672ba171fa519e54eb3440d4c3
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2024-01-30 08:40:36 +0100

    feat: sidenav with archive subsites

--- app/_components/Sidenav.tsx
+++ app/_components/Sidenav.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { redirect } from "next/navigation";
+import Link from "next/link";
+import { useState } from "react";
 import { FiX } from "react-icons/fi";
 
 export default function Sidenav({
@@ -9,6 +10,7 @@ export default function Sidenav({
   open: boolean;
   setOpen: (open: boolean) => void;
 }) {
+  const [openArchiv, setOpenArchiv] = useState(false);
   return (
     <div
       className={
@@ -26,18 +28,48 @@ export default function Sidenav({
         >
           <FiX size={30} />
         </button>
-        <button className="mx-auto mt-5 text-xl" onClick={() => redirect("/")}>
+        <Link href={"/"} className="mx-auto mt-5 text-xl">
           Home
-        </button>
-        <button className="mx-auto mt-5 text-xl" onClick={() => redirect("/")}>
+        </Link>
+        <Link href={"/mitmachen"} className="mx-auto mt-5 text-xl">
           Mitmachen
-        </button>
-        <button className="mx-auto mt-5 text-xl" onClick={() => redirect("/")}>
+        </Link>
+        <Link href={"/spenden"} className="mx-auto mt-5 text-xl">
           Spenden
-        </button>
-        <button className="mx-auto mt-5 text-xl" onClick={() => redirect("/")}>
+        </Link>
+        <button
+          onClick={() => setOpenArchiv(!openArchiv)}
+          className="mx-auto mt-5 text-xl"
+        >
           Archiv
         </button>
+
+        <div
+          className={
+            openArchiv
+              ? "flex flex-col opacity-100 transition-all"
+              : "invisible flex flex-col opacity-0 transition-all"
+          }
+        >
+          <Link
+            href={"/archive/2023"}
+            className="mx-auto mt-2 w-16 border-t border-black text-center text-xl"
+          >
+            2023
+          </Link>
+          <Link
+            href={"/archive/2021"}
+            className="mx-auto mt-1 w-16 text-center text-xl"
+          >
+            2021
+          </Link>
+          <Link
+            href={"/archive/2020"}
+            className="mx-auto mt-1 w-16 text-center text-xl"
+          >
+            2020
+          </Link>
+        </div>
       </div>
     </div>
   );
