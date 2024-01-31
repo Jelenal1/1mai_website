fb03cde3659b4d4f3666395afd648e3a94c1108b
jelenal1 <jabbruh10@gmail.com>
2024-01-31 22:31:33 +0100

    feat: client side editing is working

--- app/_components/AdminBlogEdit.tsx
+++ app/_components/AdminBlogEdit.tsx
@@ -10,64 +10,59 @@ export default function AdminBlogEdit({
   articleData: article;
 }) {
   const [onEdit, setOnEdit] = useState(false);
-
-  if (onEdit) {
-    return (
-      <main className="mx-9 mb-10 flex flex-col items-center">
-        <div className="relative flex max-w-[800px] flex-col items-center">
+  const [title, setTitle] = useState(articleData.title);
+  const [description, setDescription] = useState(articleData.description);
+  const [content, setContent] = useState(articleData.content);
+  const [author, setAuthor] = useState(articleData.author);
+  const rows = content.split("\n").length;
+  return (
+    <main className="mx-9 mb-10 flex flex-col items-center">
+      <div className="relative flex max-w-[800px] flex-col items-center">
+        {onEdit ? (
           <button
             className="absolute -right-2.5 top-5 text-4xl"
             onClick={() => setOnEdit(false)}
           >
             ✅
           </button>
-          <input
-            type="text"
-            name="title"
-            placeholder="Titel"
-            className="mt-2 text-2xl font-bold lg:text-3xl"
-          />
-          <input
-            type="text"
-            name="author"
-            placeholder="Autor"
-            className="text-sm"
-          />
-          <input
-            type="text"
-            name="image"
-            placeholder="Bild"
-            className="text-sm"
-          />
-          <textarea
-            name="content"
-            defaultValue={articleData.content}
-            className="my-2 h-96 w-full rounded-xl border-2 border-black bg-transparent p-2 text-lg outline-none"
-            onInput={(e: React.FormEvent<HTMLTextAreaElement>) => {
-              const target = e.currentTarget as HTMLTextAreaElement;
-              if (target) {
-                target.style.height = "auto";
-                target.style.height = `${target.scrollHeight}px`;
-              }
-            }}
-          />
-        </div>
-      </main>
-    );
-  }
-  return (
-    <main className="mx-9 mb-10 flex flex-col items-center">
-      <div className="relative flex max-w-[800px] flex-col items-center">
-        <button
-          className="absolute -right-2.5 top-5 text-4xl"
-          onClick={() => setOnEdit(true)}
+        ) : (
+          <button
+            className="absolute -right-2.5 top-5 text-4xl"
+            onClick={() => setOnEdit(true)}
+          >
+            ✏️
+          </button>
+        )}
+        <h1
+          className="mt-2 text-2xl font-bold focus-within:outline-none lg:text-3xl"
+          contentEditable={onEdit}
+          suppressContentEditableWarning
+          onInput={(e) => {
+            setTitle(e.currentTarget.innerText);
+          }}
         >
-          📝
-        </button>
-        <h1 className="mt-2 text-2xl font-bold lg:text-3xl">
-          {articleData.title}
+          {title}
         </h1>
-        <h2 className="text-sm">{articleData.author}</h2>
+        <h2
+          className="text-sm focus-within:outline-none"
+          contentEditable={onEdit}
+          suppressContentEditableWarning
+          onInput={(e) => {
+            setAuthor(e.currentTarget.innerText);
+          }}
+        >
+          {articleData.author}
+        </h2>
+        <h3
+          className="text-sm focus-within:outline-none"
+          contentEditable={onEdit}
+          suppressContentEditableWarning
+          onInput={(e) => {
+            setDescription(e.currentTarget.innerText);
+          }}
+        >
+          {description}
+        </h3>
         {articleData.imageurl ? (
           <img
             src={articleData.imageurl}
@@ -83,11 +78,22 @@ export default function AdminBlogEdit({
             className="my-2"
           />
         )}
-        {articleData.content.split("\n").map((paragraph, index) => (
-          <p key={index} className="my-2 w-full">
-            {paragraph}
-          </p>
-        ))}
+        {onEdit ? (
+          <textarea
+            className="my-2 w-full resize-none border-none bg-transparent focus-within:outline-none"
+            value={content}
+            rows={parseInt(rows.toString()) + 5}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+          />
+        ) : (
+          content.split("\n").map((paragraph, index) => (
+            <p key={index} className="my-2 w-full">
+              {paragraph}
+            </p>
+          ))
+        )}
       </div>
     </main>
   );
