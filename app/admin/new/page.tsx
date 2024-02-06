261057002564d3428300c1879a0dd57d6b19efa9
jelenal1 <jabbruh10@gmail.com>
2024-02-06 14:56:21 +0100

    Hope this works

--- app/admin/new/page.tsx
+++ app/admin/new/page.tsx
@@ -1,16 +1,18 @@
+"use client";
 import AdminForm from "@/app/_components/AdminForm";
-import { auth, db } from "@/app/_components/firebase";
+import { auth } from "@/app/_components/firebase";
 import { onAuthStateChanged } from "firebase/auth";
-import { addDoc, collection } from "firebase/firestore";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function page() {
-  onAuthStateChanged(auth, (user) => {
-    if (!user) {
-      redirect("/admin/login");
-    }
-  });
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        redirect("/admin/login");
+      }
+    });
+  }, [auth]);
   return (
     <main className="mx-2 mb-10">
       <AdminForm />
