a1008bc6feecf05e9764cecdf51a673e619315b5
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2024-02-13 18:45:40 +0100

    style: spacing

--- app/_components/Blogpostpreview.tsx
+++ app/_components/Blogpostpreview.tsx
@@ -2,7 +2,7 @@ import { article } from "../blog/page";
 
 export default function Blogpostpreview({ article }: { article: article }) {
   return (
-    <div className="flex min-w-[300px] max-w-[400px] flex-col rounded-xl border border-black p-2 text-center">
+    <div className="mb-6 flex min-w-[300px] max-w-[400px] flex-col rounded-xl border border-black p-2 text-center">
       <h2 className="text-xl">{article.title}</h2>
       <h3>{article.author}</h3>
       <h4 className="text-sm">{article.date}</h4>
